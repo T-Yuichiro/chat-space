74c4f122477c28c33befe5d0412fba1f4432366c
T-Yuichiro <yu1ro.t4chib4n4@gmail.com>
2019-08-21 01:44:20 +0900

    add
    非同期通信でメッセージ機能を実装した。
    
    ・jbuilderを使用して返すデータを作成した。
    ・返ってきたJSONをdoneメソッドで受け取り、HTMLを作成した。
    ・作成したHTMLをメッセージ画面の一番下に追加し、かつ画面下までスクロールするようにした。
    ・連続でボタンを押せるようにした。
    ・非同期に失敗した場合、エラーが出るようにした。

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,5 +1,29 @@
 $(function(){
-  $('form').on('submit', function(e){
+  function buildHTML(message){
+    var MessageImage = ``
+    if(message.image){
+      MessageImage =`<img class="lower-message__image" src="${message.image}">`
+    }
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                   </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                     ${message.content}
+                    </p>
+                    ${MessageImage}
+                  </div>
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -10,5 +34,16 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.form__message').val('')
+      $('hidden').val('')
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
+});
