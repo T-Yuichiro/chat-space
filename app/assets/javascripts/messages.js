3f0ccef58ad15de98e7886594fffa88a25c4e699
T-Yuichiro <yu1ro.t4chib4n4@gmail.com>
2019-08-21 20:57:08 +0900

    Update messages.js
    resetメソッドを用いて、メッセージ送信後、フォームのデータを空にするようにした。

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -35,8 +35,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html)
-      $('.form__message').val('')
-      $('hidden').val('')
+      $('#new_message')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function(){
